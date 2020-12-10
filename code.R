40a2f55f8ac84fd5da6ec9dad2dd9d9cc613d0ef
lucasgorak <lg8503a@student.american.edu>
2020-12-10 00:06:31 -0500

    Update1

--- code.R
+++ code.R
@@ -15,14 +15,11 @@ colnames(fec_raw) <- as.character(fec_raw[4, ])
 fec_raw <- fec_raw[-c(1:4), ]
 fec_raw <- fec_raw %>% select(-`Coverage End Date`)
 
-fec_rename <-
+fec_data <-
   fec_raw %>% rename(
     "candidate" = Candidate,
-    "state" = State,
-    "district" = District,
-    "party" = Party,
+    "can_party" = Party,
     "receipts" = Receipts,
-    "party" = Party,
     "ico" = `Incumbent/\r\nChallenger/Open`,
     "idv_contr" = `Contributions \r\nfrom Individuals`,
     "pac_contr" = `Contributions\r\nfrom PACs and\r\nOther Committees`,
@@ -32,7 +29,7 @@ fec_rename <-
     "debts" = Debts) # Rename columns
 
 fec_data <-
-  fec_rename %>% mutate(district = as.numeric(district),
+  fec_data %>% mutate(district = as.numeric(district),
                         receipts = as.numeric(receipts),
                         idv_contr = as.numeric(idv_contr),
                         pac_contr = as.numeric(pac_contr), 
@@ -43,8 +40,8 @@ fec_data <-
 
 fec_data <-
   fec_data %>% mutate(
-    party = recode(
-      party,
+    can_party = recode(
+      can_party,
       `Republican Party` = "republican",
       `Democratic Party` = "democrat",
       `Independent` = "independent", 
@@ -72,21 +69,85 @@ fec_data <- fec_data %>% na_if("NA") # Make sure anything NA is NA.
 election_raw <- read_csv("https://raw.githubusercontent.com/lgorak/finalproject/main/rawdata/MITLab_Electionresults_1976-2018.csv")
 election_raw <- election_raw %>% filter(year == "2018")
 election_raw <- election_raw %>% filter(writein == "FALSE")
-election_2018 <- election_raw %>% select(state, state_po, )
+election_2018 <-
+  election_raw %>% select("state" = state_po,
+                          district,
+                          candidate,
+                          candidatevotes,
+                          totalvotes)
 
 ## Election Winner Data ##
 
 win_raw <- read_csv("https://raw.githubusercontent.com/lgorak/finalproject/main/rawdata/CookPR_Electionwinner_2-18.csv")
+win_2018 <-
+  win_raw %>% select("district" = Dist,
+                     "win_can" = `2018 Winner`,
+                     "dem_votes" = `Dem Votes`,
+                     "rep_votes" = `GOP Votes`,
+                     "dem_perc" = `Dem %`,
+                     "rep_perc" = `GOP %`,
+                     "dem_margin" = `Dem Margin`) # Select rows I want
+
+win_2018 <- win_2018 %>% mutate("rep_margin" = dem_margin * -1) # New column: margin of win by GOP
+
+win_2018 <- win_2018 %>% separate(win_can, c("win_can", "win_party"), " \\(") 
+win_2018 <-
+  win_2018 %>% mutate(win_party = recode(win_party, `R)` = "republican", `D)` = "democrat")) # New column: winning party
+
+win_2018 <- win_2018 %>% separate(district, c("state", "district"), "-")
+win_2018 <- win_2018 %>% mutate(district = recode(district, `AL` = "0", .default = district)) #Separate district columns and fix At-Large problem
+win_2018 <- win_2018 %>% mutate(district = as.numeric(district))
 
 ## District Data Wrangling ##
 
 context_raw <- read_csv("https://raw.githubusercontent.com/lgorak/finalproject/main/rawdata/FiveThirtyEight_Electioncontext_2018.txt")
 context_raw <- context_raw %>% filter(maptype == "current")
+context_2018 <-
+  context_raw %>% select(
+    state,
+    district,
+    "dist_population" = population,
+    "dist_population_18_over" = population_18_over,
+    "dist_cookPVI" = PVI,
+    "dist_white" = `Non-Hispanic White`,
+    "dist_africanamerican" = `African-American`,
+    "dist_latino" = `Hispanic/Latino`,
+    "dist_asian" = `Asian`,
+    "dist_nativeamerican" = `Native American`,
+    "dist_pacificislander" = `Pacific Islander`,
+    "dist_other" = `Other`,
+    "dist_race_category" = race_category)
+context_2018 <- context_2018 %>% mutate(district = as.numeric(district))
 
-## Parse names in each dataset using humaniformat ##
+## Parse names in all data ##
 
+fec_data_fixnames <- fec_data %>% separate(candidate, c("lastname", "firstname"), "\\, ")
+fec_data_fixnames <- fec_data_fixnames %>% separate(firstname, c("firstname", "extra"), "\\ ")
+fec_data_fixnames <- fec_data_fixnames %>% separate(lastname, c("lastname", "extra"), "\\ ")
+fec_data_fixnames <- fec_data_fixnames %>% mutate(candidate = paste(firstname, lastname))
+fec_data_fixnames$candidate <- fec_data_fixnames$candidate %>% str_replace_all("[[:punct:]]", "")
+fec_data_fixnames <- fec_data_fixnames %>% select(-c(lastname, firstname, extra))
 
+candidatenames <- election_2018$candidate %>% parse_names()
+election_2018_fixnames <- full_join(election_2018, candidatenames, by = c("candidate" = "full_name"))
+election_2018_fixnames <- election_2018_fixnames %>% mutate(candidate = paste(toupper(first_name), toupper(last_name)))
+election_2018_fixnames$candidate <- election_2018_fixnames$candidate %>% str_replace_all("[[:punct:]]", "")
+election_2018_fixnames <- election_2018_fixnames %>% select(-c(salutation, first_name, middle_name, last_name, suffix))
 
-# fec_data_pres <- fec_data_pres %>% mutate(full_name = format_reverse(candidate))
+win_2018_fixnames <- win_2018 %>% separate(win_can, c("firstname", "lastname"), "\\ ")
+win_2018_fixnames <- win_2018_fixnames %>% separate(firstname, c("firstname", "extra"), "\\ ")
+win_2018_fixnames <- win_2018_fixnames %>% separate(lastname, c("lastname", "extra"), "\\ ")
+win_2018_fixnames <- win_2018_fixnames %>% mutate(win_can = paste(toupper(firstname), toupper(lastname)))
+win_2018_fixnames$win_can <- win_2018_fixnames$win_can %>% str_replace_all("[[:punct:]]", "")
+win_2018_fixnames <- win_2018_fixnames %>% select(-c(lastname, firstname, extra))
 
+## Combine Data ##
+
+context_win_2018 <- win_2018_fixnames %>% full_join(context_2018, by = c("state", "district")) #Combine winner data with context data using congressional district
+votes_win_context <- context_win_2018 %>% left_join(election_2018_fixnames, by = c("state", "district"))
+votes_win_context_FEC <- votes_win_context %>% left_join(fec_data_fixnames, by = "candidate")
+
+## Combine and Clean Everything ##
+
+votes_win_context_FEC <- votes_win_context_FEC %>% mutate(winner = (win_can == candidate))
 
